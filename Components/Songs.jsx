623b02ae4fe462f4c8fb2e4dbd4c3f36981414a8
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-05 06:47:45 -0500

    makes some preliminary changes to pages and components

--- Components/Songs.jsx
+++ Components/Songs.jsx
@@ -1,1 +1,26 @@
 import { useState, useEffect } from "react";
+import Song from "./Song";
+
+const API = import.meta.env.VITE_API_URL;
+
+function Songs() {
+  const [songs, setSongs] = useState([]);
+  useEffect(() => {
+    fetch(`${API}/songs`)
+      .then(response => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON);
+        setSongs(responseJSON.data.payload);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className="Songs">
+      
+    </div>
+  );
+}
+
+export default Songs;
---
+++ Pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Rock Music App</h2>
+      <p>Your one-stop destination for everything rock!</p>
+      <p>Discover the best rock songs, artists, and more.</p>
+      <p>Join our community of rock enthusiasts and explore the world of rock music.</p>
+    </div>
+  );
+}
+
+export default Home;
