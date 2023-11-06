f2eb30283f3164ceba76428e78d956c045f5caed
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-05 20:32:10 -0500

    adds underline to home

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home-container">
       <div className="content">
-        <h2>Welcome to the Rock Music Hub</h2>
+        <h2 className='underline'>Welcome to the Rock Music Hub</h2>
         <p>Your ultimate destination for rock music enthusiasts.</p>
         <p>Discover the iconic world of rock and connect with fellow fans.</p>
       </div>
